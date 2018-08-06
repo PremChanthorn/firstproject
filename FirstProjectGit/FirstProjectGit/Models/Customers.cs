240e36382f0ed4e47417eda94e5b2fda7e5ecdef
USER <premchanthorn007@gmail.com>
2018-08-06 22:42:53 +0700

    add customers

---
+++ FirstProjectGit/FirstProjectGit/Models/Customers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstProjectGit.Models
+{
+    public class Customers
+    {
+        public int CustomerId { get; set; }
+        public String CustomeName { get; set; }
+        public string CustomerGender { get; set; }
+    }
+}
