cb13ff53383738699d90c17d73659c08bfaec0d5
USER <premchanthorn007@gmail.com>
2018-08-06 21:39:18 +0700

    first commit

---
+++ FirstProjectGit/FirstProjectGit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstProjectGit.MvcApplication" Language="C#" %>
---
+++ FirstProjectGit/FirstProjectGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstProjectGit.Startup))]
+namespace FirstProjectGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
