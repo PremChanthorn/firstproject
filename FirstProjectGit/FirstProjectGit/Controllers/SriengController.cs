b23995209d1494fe06469ae2ee7f2d8a4cddfa24
homsriang@gmail.com <homsriang@gmail.com>
2018-08-07 21:37:22 +0700

    Add srieng controller

---
+++ FirstProjectGit/FirstProjectGit/Controllers/SriengController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstProjectGit.Controllers
+{
+    public class SriengController : Controller
+    {
+        // GET: Srieng
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
